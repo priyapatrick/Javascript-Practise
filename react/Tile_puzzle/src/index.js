e5d8eaa12c374a90d001e60407dcd131983bbe54
priyapatrick <priya03081999@gmail.com>
2021-08-19 16:36:08 -0700

    react tile puzzle

---
+++ react/Tile_puzzle/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Tilegame from './puzzle';
+
+ReactDOM.render(
+    <Tilegame />,
+  document.getElementById('root')
+);
