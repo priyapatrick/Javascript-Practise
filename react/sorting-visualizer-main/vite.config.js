cdab5c866b07af4e2a1d44606f79c4a2e8dd1f7b
priyapatrick <priya03081999@gmail.com>
2021-08-19 17:22:10 -0700

    updated

--- react/sorting-visualizer-main/vite.config.js
+++ react/sorting-visualizer-main/vite.config.js
@@ -3,7 +3,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  hmr: { overlay: false },
+  server:{
+    hmr:{
+      overlay:false
+    }
+  },
   plugins: [reactRefresh()],
   build: {
     target: 'es2018'
